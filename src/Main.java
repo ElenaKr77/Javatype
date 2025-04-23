d0c52031853ef3ae2edaed12504750a23ed5c9e2
ElenaKr77 <elena.kriventsova77@gmail.com>
2025-04-23 21:33:25 +0300

    Update src2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println( " Программа:бесплатные мили за перелет. " );
-    int ticket = 21000;
-        System.out.println( " Стоимость билета: 21000 " );
+    int ticket = 18000;
+        System.out.println( " Стоимость билета: " + ticket );
      int mile = 30;
         System.out.println( " 1 миля 30 рублей " );
      int bonus = ticket / mile;
         System.out.println( " Начислено милей " + bonus );
-        
+
     }
 }
